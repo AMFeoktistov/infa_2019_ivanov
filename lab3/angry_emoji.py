9d729d3503f0bc09fde2ac20c8c5222c6a0349fb
AMFeoktistov <artiomfeoktistov@gmail.com>
2019-11-04 17:51:57 +0300

    Два первых задания

---
+++ lab3/angry_emoji.py
@@ -0,0 +1,17 @@
+from graph import *
+
+penColor('black')
+brushColor('yellow')
+circle(300, 300, 100)  # face
+brushColor('red')
+circle(250, 270, 20)  # white of the eyes
+circle(350, 270, 15)
+brushColor('black')
+circle(250, 270, 8)  # pupils
+circle(350, 270, 8)
+penSize(10)
+line(280, 270, 200, 200)  # eyebrows
+line(320, 270, 400, 220)
+penSize(20)
+line(260, 360, 340, 360)  # mouth
+run()
--- lab3/example1.py
+++ lab3/example1.py
@@ -3,8 +3,13 @@ from graph import *
 
 penColor('black')
 penSize(5)
-brushColor(0, 100, 189)
-polygon([(100, 100), (300, 200),
-         (400,500), (100 ,200), (100, 100)])
-
+brushColor('brown')
+polygon([(100, 100), (200, 50), (300, 100)])
+brushColor(200, 50, 150)
+rectangle(100, 100, 300, 200)
+brushColor('yellow')
+penColor('white')
+circle(200, 150, 30)
+line(230, 150, 170, 150)
+line(200, 120, 200, 180)
 run()
